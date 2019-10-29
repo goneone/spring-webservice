50816a2915796161d0da4c30167deefe2458156a
goneone <kuyt1819@gmail.com>
2019-10-29 16:49:37 +0900

    git commit

---
+++ src/main/java/com/kuyt1819/webservice/domain/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.kuyt1819.webservice.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long>{
+	//단순히 인터페이스를 생성후, JpaRepository<Entity클래스, PK타입>를 상속하면 기본적인 CRUD 메소드가 자동생성 됨
+}
