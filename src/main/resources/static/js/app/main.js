8dbefeacc5f09d1e72c2db7510663b35bc852479
goneone <kuyt1819@gmail.com>
2019-12-21 16:37:15 +0900

    mustache 시작

--- src/main/resources/static/js/app/main.js
+++ src/main/resources/static/js/app/main.js
@@ -16,15 +16,15 @@ var main = {
 			console.log("abcd")
 			$.ajax({
 	            type: 'POST',
-	            url: '/posts',
+	            url: '/api/v1/posts',
 	            dataType: 'json',
 	            contentType:'application/json; charset=utf-8',
 	            data: JSON.stringify(data)
 	        }).done(function() {
 	            alert('글이 등록되었습니다.');
-	            location.reload();
+	            window.location.href = '/'; //
 	        }).fail(function (error) {
-	            alert(error);
+	            alert(JSON.stringify(error));
 	        });
 		}
 };
