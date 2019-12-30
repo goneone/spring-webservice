c7b7d8975623891b69b01cafcd33377281dfd349
goneone <kuyt1819@gmail.com>
2019-12-30 16:29:48 +0900

    real-auth 구현

--- src/main/java/com/kuyt1819/webservice/web/HelloController.java
+++ src/main/java/com/kuyt1819/webservice/web/HelloController.java
@@ -1,10 +1,11 @@
 package com.kuyt1819.webservice.web;
 
 
+import com.kuyt1819.webservice.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-// 컨트롤러를 json반환하는 컨트롤러로 만들어줌
 @RestController
 public class HelloController {
 
@@ -12,8 +13,14 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
-}
 
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
+}
 //    @RequestParam : 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
 //
 //    위의 코드에서는 외부에서 name (@RequestParam("name")) 이란 이름으로 넘긴 파라미터를 메소드 파라미터 name(String name)에 저장하기 됨.
