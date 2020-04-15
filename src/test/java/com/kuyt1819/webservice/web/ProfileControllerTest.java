e4fc386d8ceb475e9fdce41f88ebb9ad79bed9d1
goneone <kuyt1819@gmail.com>
2020-04-15 22:50:01 +0900

    20200415 nginx let's go

--- src/test/java/com/kuyt1819/webservice/web/ProfileControllerTest.java
+++ src/test/java/com/kuyt1819/webservice/web/ProfileControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+//test
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProfileControllerTest {
